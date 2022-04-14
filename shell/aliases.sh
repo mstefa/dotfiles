8394a6899f34fc88ff3193c3828e3396c602b3af
mstefanutti <mstefanutti24@gmail.com>
2022-04-13 22:54:57 -0300

    feat: add command aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -10,17 +10,23 @@ alias dotfiles='cd $DOTFILES_PATH'
 
 # Git
 alias gaa="git add -A"
-alias gc='$DOTLY_PATH/bin/dot git commit'
+alias ga="git add"
+alias gc='git commit'
+alias gsw='git swith'
+alias gs="git status"
 alias gca="git add --all && git commit --amend --no-edit"
-alias gco="git checkout"
-alias gd='$DOTLY_PATH/bin/dot git pretty-diff'
+alias gd='git pretty-diff'
 alias gs="git status -sb"
 alias gf="git fetch --all -p"
-alias gps="git push"
+alias gp="git push"
 alias gpsf="git push --force"
 alias gpl="git pull --rebase --autostash"
 alias gb="git branch"
 alias gl='$DOTLY_PATH/bin/dot git pretty-log'
+
+# Docker
+alias doc = "docker-compose"
+
 
 # Utils
 alias k='kill -9'
