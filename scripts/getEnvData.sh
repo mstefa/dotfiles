658ecd87583ba9cc5798e261361d4508664bd960
mstefa <mstefanutti24@gmail.com>
2023-02-28 20:48:09 +0100

    add kube scripts

--- scripts/getEnvData.sh
+++ scripts/getEnvData.sh
@@ -2,5 +2,5 @@
 
 #kubectl -n $1 get secret aura-configuration-api -o json
 
-(kubectl -n $1 get cm aura-configuration-api -o json | jq -r ".data|to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]"; kubectl -n $1 get secret aura-configuration-api -o json | jq -r ".data|to_entries|map(\"\(.key)=\(.value|tostring|@base64d)\")|.[]") | sort
+(kubectl -n "$1" get cm aura-configuration-api -o json | jq -r ".data|to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]"; kubectl -n "$1" get secret aura-configuration-api -o json | jq -r ".data|to_entries|map(\"\(.key)=\(.value|tostring|@base64d)\")|.[]") | sort
 
