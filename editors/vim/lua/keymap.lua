b1bec93a5c99e7de41509bb7ec5ffe07d07e580f
Matias Stefanutti <mstefanutti24@gmail.com>
2026-05-25 21:59:57 -0300

    fix

--- editors/vim/lua/keymap.lua
+++
@@ -1,6 +0,0 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
-vim.keymap.set("i", "jj", "<Esc>")
-vim.keymap.set("n", "<leader>w", "<cmd>w<CR>")
-vim.keymap.set("n", "<leader>q", "<cmd>q<CR>")
